fe53cc0232d680e1fddf960c587963e108e1b99e
Sadik Hassan <sadikhassan234@gmail.com>
2021-02-07 20:13:33 +0600

    student full

--- Student class/Student class/Program.cs
+++ Student class/Student class/Program.cs
@@ -12,16 +12,72 @@ namespace Student_class
         private string id;
         private string dept;
         private float cgpa;
+        public Student()
+        {
+
+        }
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string Id
+        {
+            set
+            {
+                id = value;
+            }
+            get
+            {
+                return id;
+            }
+        }
+        public float Cgpa
+        {
+            set
+            {
+                cgpa = value;
+            }
+            get
+            {
+                return cgpa;
+            }
+        }
+        public string Dept
+        {
+            set
+            {
+                dept = value;
+            }
+            get
+            {
+                return dept;
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Id : " + Id);
+            Console.WriteLine("Cgpa : " + Cgpa);
+            Console.WriteLine("Depertment : " + Dept);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Student s1 = new Student();
-            s1.name = "SADIK HASSAN";
-            s1.id = "18-36843-1";
-            s1.dept = "CSE";
-            s1.cgpa = 3.07f;
+            s1.Name = "SADIK HASSAN";
+            s1.Id = "18-36843-1";
+            s1.Dept = "CSE";
+            s1.Cgpa = 3.07f;
+            s1.show();
 
 
         }
