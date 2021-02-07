f5e056a2ac7b5d12d97d3a781c012205acb00bb0
Sadik Hassan <sadikhassan234@gmail.com>
2021-02-07 11:58:56 +0600

    lab-2_final

--- five subject/five subject/Program.cs
+++ five subject/five subject/Program.cs
@@ -12,8 +12,32 @@ namespace five_subject
         {
             int phy=62, che=83, bio=93, math=89, com=71;
             double sum = phy + che + bio + math + com;
-            Console.WriteLine("percentage is " + sum / 5.0 +"%");
-
+            sum /= 5.0;
+            Console.WriteLine("percentage is " + sum +"%");
+            if(sum<50)
+            {
+                Console.WriteLine("Failed");
+            }
+            else if(sum>=50&&sum<75)
+            {
+                Console.WriteLine("C+");
+            }
+            else if(sum>=75&&sum<=79)
+            {
+                Console.WriteLine("B");
+            }
+            else if(sum>=80&&sum<85)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (sum >= 85 && sum < 89)
+            {
+                Console.WriteLine("A");
+            }
+            else
+            {
+                Console.WriteLine("A+");
+            }
         }
     }
 }
